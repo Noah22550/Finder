b599bed3cbea6a2a927f68761df6fea48e601ad1
Noah22550 <noahdepagne@gmail.com>
2026-09-15 23:30:48 +0200

    seed faite

---
+++ Finder/api/prisma/seed.js
@@ -0,0 +1,88 @@
+import { readFileSync, stat } from 'node:fs';
+import path from 'node:path';
+import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
+const DATA_DIR = path.join(import.meta.dirname, '..', 'finder-data');
+const lire = (fichier) => JSON.parse(readFileSync(path.join(DATA_DIR,fichier), 'utf8'));
+
+async function main() {
+    const chambres = lire('chambres.json');
+    const comptes = lire('comptes.json');
+    const hotels = lire('hotels.json');
+    const reservations = lire('reservations.json')
+
+    await prisma.reservations.deleteMany();
+    await prisma.comptes.deleteMany();
+    await prisma.chambres.deleteMany();
+    await prisma.hotels.deleteMany();
+
+    await prisma.hotels.createMany({
+        data: hotels.map((h)=>({
+            id: h.id,
+            nom: h.nom,
+            etoiles: h.etoiles,
+            adresse: h.adresse,
+            code_postal: h.code_postal,
+            ville: h.ville,
+            telephone: h.telephone,
+            email: h.email,
+            gerant: h.gerant,
+            description: h.description
+        })
+    )
+    })
+
+    await prisma.chambres.createMany({
+        data: chambres.map((l) => ({
+            id: l.id,
+            hotel_Id: l.hotel_id,
+            numero: l.numero,
+            categorie: l.categorie,
+            capacite: l.capacite,
+            prix_nuit: l.prix_nuit,
+            description:l.description,
+            disponible: l.disponible
+        })),
+    });
+    const comptesHaches = await Promise.all(
+        comptes.map(async(c) => ({
+                id: c.id,
+                role: c.role,
+                email: c.email,
+                mot_de_passe_clair: await bcrypt.hash(c.mot_de_passe_clair, 10),
+                nom: c.nom,
+                prenom: c.prenom,
+                hotel_Id: c.hotel_id ?? null,
+                telephone: c.telephone ?? null
+
+            }))
+    );
+    await prisma.comptes.createMany({data: comptesHaches})
+    
+    await prisma.reservations.createMany({
+        data: reservations.map((r)=>({
+            id: r.id,
+            voyageur_id: r.voyageur_id,
+            chambre_id: r.chambre_id,
+            date_arrivee: new Date(r.date_arrivee),
+            date_depart: new Date(r.date_depart),
+            nb_personnes: r.nb_personnes,
+            statut: r.statut,
+            demande_special: r.demande_special ?? null
+        }))
+    })
+    console.log(`${chambres.length} chambres, ${comptes.length} comptes,
+${hotels.length} hoptels, ${reservations.length} reservations`);
+}
+
+
+main()
+    .catch((erreur) => {
+    console.error(erreur);
+    process.exit(1);
+ })
+
+ .finally(() => prisma.$disconnect())
